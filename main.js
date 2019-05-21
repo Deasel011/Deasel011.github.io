075f89fb95019bc85d019b6277d60271e4108738
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2019-05-20 20:50:25 -0400

    renewal!

--- main.js
+++ main.js
@@ -1,17 +1,96 @@
-strings = {
-    "bio": {
-        "text": {
-            "fr": "Je suis étudiant au BAC en informatique à l'Université du Québec à Montréal. Je possède un certificat en Études Videoludiques de l'Université de Montréal et je n'ai jamais terminer une majeure en Histoire dans ce même établissement... Je suis un passionné de jeux vidéos, mais n'aspire pas à en créer... je préfere utiliser et développer des méthodes de triches et d'automatisation dans ces-derniers! :)",
-            "en": "I am currently studying Computer Science at Université du Québec à Montréal. I have a certificate in Video Game Studies from University of Montreal and still have History major pending (probably dead by now)... I love video games but would never aspire to create one... I prefer to use and develop methods of cheating such as automaton! :)"
+var language;
+language = window.navigator.language.substring(0,2) || "en";
+
+
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
         }
-    }
+    };
+    xhr.send();
 };
 
-function loadText(lang) {
-    text = document.querySelector(".propos p");
-    text.innerText = strings.bio.text[lang];
+var getHtml = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'text';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+function setLanguageButton(lang){
+    currentLang = lang;
+    document.getElementById("language-button").innerText = lang;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    loadText("fr")
+function useLanguage(lang){
+    loadText(lang);
+    setLanguageButton(lang);
+}
+
+function swapLanguage() {
+    switch (currentLang) {
+        case "en":
+            useLanguage("fr");
+            break;
+        case "fr":
+            useLanguage("en");
+            break;
+        default:
+            break;
+    }
+}
+
+function setAPropos(lang){
+    apropos = document.querySelector(".propos");
+    getJSON("string.json",function(status,data){
+
+        while (apropos.firstChild) {
+            apropos.removeChild(apropos.firstChild);
+        }
+
+        h = document.createElement("h1");
+        h.innerText = data.about.label[lang];
+        apropos.appendChild(h);
+
+        for(var paragraph in data.about.text[lang]){
+            p = document.createElement("p");
+            p.innerText = data.about.text[lang][paragraph];
+            apropos.appendChild(p);
+        }
+    });
+}
+
+function loadText(lang) {
+    setAPropos(lang)
+
+}
+
+function loadMenu(callback) {
+    getHtml("_menu.html",function(status,html){
+        document.getElementById("nav-container").innerHTML = html;
+        callback();
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadMenu(
+        function(){useLanguage(language)});
+
 });
+
+
